1fc2dbfafcb3261c5d01d5d3804e4ee9de958232
edwwlui <rewq142857@hotmail.com>
2021-10-29 13:44:46 +0800

    update

--- scripts/amplicon/filter_bam.py
+++ scripts/amplicon/filter_bam.py
@@ -10,21 +10,22 @@ def filter_bam(bam,AS_threshold=0,MAPQ_threshold=0,AS_LEN_ratio=0,if_AS_and_MAPQ
         suffix=f'AS{AS_threshold}_AS-LEN_ratio{AS_LEN_ratio}'
     bam_path=f'{os.path.basename(bam)}.{suffix}.bam'
     with pysam.AlignmentFile(bam, "rb") as samfile, pysam.AlignmentFile(bam_path,'wb',template=samfile) as fo:
-        for read in samfile.fetch():
-            if if_AS_and_MAPQ_filter:
+
+        if if_AS_and_MAPQ_filter:
+            for read in samfile.fetch():
                 if read.get_tag('AS')>=AS_threshold and read.mapping_quality>=MAPQ_threshold:
                     fo.write(read)
-            else:
-                #  AS_and_AS-LEN_ratio_filter
-                with open(f'{bam_path}.ASandAS-LEN_ratio_filter.readid','w') as out_list:
-                    readid_set=set()
-                    for read in samfile.fetch():
-                        if read.get_forward_sequence() == None:
-                            continue
-                        if read.get_tag('AS')>=AS_threshold and read.get_tag('AS')/len(read.get_forward_sequence())>=AS_LEN_ratio:
-                            fo.write(read)
-                            readid_set.add(read.qname)
-                    out_list.write('\n'.join(readid_set))
+        else:
+            #  AS_and_AS-LEN_ratio_filter
+            with open(f'{bam_path}.ASandAS-LEN_ratio_filter.readid','w') as out_list:
+                readid_set=set()
+                for read in samfile.fetch():
+                    if read.get_forward_sequence() == None:
+                        continue
+                    if read.get_tag('AS')>=AS_threshold and read.get_tag('AS')/len(read.get_forward_sequence())>=AS_LEN_ratio:
+                        fo.write(read)
+                        readid_set.add(read.qname)
+                out_list.write('\n'.join(readid_set))
     os.system(f'samtools index {bam_path}')
 
 if __name__=="__main__":
